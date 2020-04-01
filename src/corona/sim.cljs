82561e4bc55d105c4ed65477e589822d6f1cadc0
vitalipe <vitali.pe@gmail.com>
2020-04-01 04:17:31 +0300

    a little less broken sim

--- src/corona/sim.cljs
+++ src/corona/sim.cljs
@@ -1,25 +1,31 @@
 (ns corona.sim
-  (:require [alpakit.util :refer [pivot-by map-vals]]))
+  (:require
+    [alpakit.util :refer [pivot-by map-vals]]
+    [corona.math  :refer [clamp **]]))
 
 
 (defrecord Person [id
                    state      ; #{:S :I :R}
-                   position   ; vector
-                   velocity]) ; vector
+                   position   ; vector-2d
+                   velocity]) ; vector-2d
 
 
-(defn spawn-uninfected-person [{:keys [w h]} id]
+(defn spawn-person [{:keys [w h]}
+                    gods-of-rng-hate-this-one?
+                    this-one-is-saved?
+                    id]
+  ;; we all live in a fucking simulation, made by shitty web programmers :(
   (map->Person {:id id
-                :state :S
                 :position [(rand-int w) (rand-int h)]
-                :velocity [(- (rand-int 6) 3) (- (rand-int 6) 3)]}))
+                :velocity [(- (rand-int 6) 3) (- (rand-int 6) 3)]
+                :state    (cond
+                            (gods-of-rng-hate-this-one? id) :I
+                            (this-one-is-saved? id)         :R
+                            :youre-not-special              :S)}))
+
 
 (def infected?    (comp (partial = :I) :state))
 (def susceptible? (comp (partial = :S) :state))
-
-
-(defn clamp [from val to]
-  (max from (min to val)))
 
 
 (defn spread-vec-2d [r [x y]]
@@ -27,19 +33,38 @@
         dy (range (inc r))]
     [(+ x dx) (+ y dy)]))
 
+(defn population->SIR% [population]
+  (let [stats (->> (vals population)
+                (map :state)
+                (frequencies)
+                (merge {:S 0 :I 0 :R 0}))]
+    (-> stats
+      (update :S / (count population))
+      (update :I / (count population))
+      (update :R / (count population)))))
+
 
 (defn naively-infect-population [infect% infect-dist population]
-  (let [targets    (->> (vals population)
+  (let [n->% (fn [n] (- 1 (** (- 1 infect%) n)))
+        targets    (->> (vals population)
                      (filter susceptible?)
                      (group-by :position))
 
-        infection-vectors (->> (vals population)
-                            (filter infected?)
-                            (map :position)
-                            (mapcat (partial spread-vec-2d infect-dist))
-                            (frequencies))]
-       ;; FIXME: do the last step here...
-       population))
+        vectors (->> (vals population)
+                  (filter infected?)
+                  (map :position)
+                  (mapcat (partial spread-vec-2d infect-dist))
+                  (frequencies))
+
+        infected-ids (->> targets
+                       (map    (fn [[v targets]] [(get vectors v 0), targets]))
+                       (map    (fn [[n targets]] [(n->% n),          targets]))
+                       (mapcat (partial apply random-sample))
+                       (map    :id))]
+      (reduce
+        (fn [population id] (assoc-in population [id :state] :I))
+        population
+        infected-ids)))
 
 
 (defn randomly-move-population [w h population]
@@ -49,29 +74,34 @@
                   :position [(clamp 0 (+ x dx) w) (clamp 0 (+ y dy) h)]
                   :velocity [(- (rand-int 6) 3) (- (rand-int 6) 3)])))))
 
+
 ;; API
-(defn init [{:keys [w h] :as config} n]
-  (let [population (->> (inc n)
-                     (range 1)
-                     (map (partial spawn-uninfected-person config))
+(defn init [{:keys [w h] :as config} [n-susceptible n-infected n-retired]]
+  (let [id-seed             (range 1 (+ 1 n-susceptible n-infected n-retired))
+        ids-of-the-infected (->> id-seed
+                              (shuffle)
+                              (take n-infected)
+                              (into #{}))
+        ids-of-the-retired (->> id-seed
+                              (shuffle)
+                              (take n-retired)
+                              (into #{}))
+        spawn      (partial spawn-person config ids-of-the-infected ids-of-the-retired)
+        population (->> id-seed
+                     (map spawn)
                      (pivot-by :id))]
+
     {:step       0
      :population population
      :config     config
-     :report {:S n
-              :I 0
-              :R 0}}))
+     :report     (population->SIR% population)}))
 
 
 (defn step [{{:keys [w h]} :config, :keys [step population] :as state}]
   (let [updated (->> population
-                   (naively-infect-population 0.2 1)
+                   (naively-infect-population 0.9 10)
                    (randomly-move-population w h))]
     (assoc state
        :step       (inc step)
        :population updated
-       :report     (merge
-                     {:S 0 :I 0 :R 0}
-                     (->> (vals updated)
-                       (map :state)
-                       (frequencies))))))
+       :report     (population->SIR% updated))))
