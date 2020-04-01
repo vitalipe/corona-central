31debf8f78051693f1452f238bc09bcddddb2883
vitalipe <vitali.pe@gmail.com>
2020-04-01 04:18:08 +0300

    WIP plot

--- src/corona/ui/plot.cljs
+++ src/corona/ui/plot.cljs
@@ -5,36 +5,75 @@
     [alpakit.widget :refer [defw]]
     [alpakit.p5 :refer [p5-canvas]]
 
-    [corona.ui.theme :refer [plot-i-color plot-s-color plot-r-color]]))
+    [corona.ui.theme :refer [plot-i-color plot-s-color plot-r-color]]
+    [corona.math  :refer [ceil round]]
+    [corona.sim :as sim]))
+
+;;; WIP
+(def demo-sim (reduce
+                (fn [s _] (conj s (sim/step (last s))))
+                [(sim/init {:w 100 :h 100} [100 1 0])]
+                (range 100)))
+
+(def demo-data (mapv :report demo-sim))
+;;;
 
 
-(def static-demo-data (->> (range 0 100 2)
-                           (mapv #(let [s (- 100 %)
-                                        i (/ (- 100 s) 2)
-                                        r i]
-                                    {:S s
-                                     :I i
-                                     :R r}))))
+(defn draw-susceptible! [ctx w h x-points]
+  (doto ctx
+    (.fill plot-s-color)
+    (.beginShape)
+    (.vertex 0 h))
+
+  (loop [[[pos-x {I :I S :S} i] & points] x-points]
+    (.vertex ctx pos-x (- h (* (+ I S) h)))
+    (when-not (empty? points) (recur points)))
+
+  (doto ctx
+    (.vertex w h)
+    (.endShape)))
+
+
+(defn draw-infected! [ctx w h x-points]
+  (doto ctx
+    (.fill plot-i-color)
+    (.beginShape)
+    (.vertex 0 h))
+
+  (loop [[[pos-x {I :I} i] & points] x-points]
+    (.vertex ctx pos-x (- h (* (+ I) h)))
+    (when-not (empty? points) (recur points)))
+
+  (doto ctx
+    (.vertex w h)
+    (.endShape)))
+
 
 (defw graph []
   [p5-canvas
      :draw (fn [ctx]
              (let [w (.-width ctx)
-                   h (.-height ctx)]
+                   h (.-height ctx)
+
+                   ;; number of x values?
+                   x-n (min
+                         (quot w 20)
+                         (count demo-data))
+
+                   ;; how far are the x values in px?
+                   xw-px (quot w  x-n)
+
+                   step-size (round (/ (count demo-data) x-n))
+
+                  ;; vector of [x-pos, {:S .. :I .. :R ..}, value]
+                   x-points (map vector
+                              (range 0 w xw-px)
+                              (take-nth step-size demo-data)
+                              (range 0 99999 step-size))]
+
                (doto ctx
                 (.background plot-r-color)
-                (.noStroke)
-                (.fill plot-i-color)
+                (.noStroke))
 
-                (.beginShape)
-                (.vertex 0 h)
-                (.vertex 50 (- h 100))
-                (.vertex 200 (- h 200))
-                (.vertex 240 (- h 200))
-                (.vertex 350 (- h 210))
-                (.vertex 460 (- h 240))
-                (.vertex 570 (- h 300))
-                (.vertex 670 (- h 100))
-                (.vertex 780 h)
-
-                (.endShape))))])
+              (draw-susceptible! ctx w h x-points)
+              (draw-infected! ctx w h x-points)))])
